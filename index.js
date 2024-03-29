849579936b0cf4b32e3f0ad3cf0ac2217f706eec
Asfaakhan <khanzadiasfi0314@gmail.com>
2024-03-29 04:44:34 -0700

    created number guseeing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answers = await inquirer.prompt([
+    {
+        message: "Please guess a number  b/w 1 to 5: ",
+        name: "userGuessNumber",
+        type: "number",
+    }
+]);
+if (answers.userGuessNumber === randomNumber) {
+    console.log("Congratulations! You guessed the right number : ");
+}
+else {
+    console.log(" Oops ! You guessed the wrong number :");
+}
