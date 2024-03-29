b3960886e1ce440d8da34db2b26e2362c3f77385
Asfaakhan <khanzadiasfi0314@gmail.com>
2024-03-29 04:52:32 -0700

    added console

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 5 + 1);
+console.log("Welcome To Number Guessing Game");
 const answers = await inquirer.prompt([
     {
         message: "Please guess a number  b/w 1 to 5: ",
